932580f72a8ba646e5a7bbd64591537d026ecd51
freeman-lab <the.freeman.lab@gmail.com>
2021-09-14 10:36:48 -0700

    merge from main

--- contents.js
+++ contents.js
@@ -4,7 +4,7 @@ const contents = [
     version: '1.0.0',
     title: 'Welcome to the blog',
     authors: ['Jeremy Freeman'],
-    date: '07-14-2021',
+    date: '09-14-2021',
     summary: "Why we made this and what you'll find here",
     number: 0,
   },
