948c8f05581340318bf0764de8a665b7747e2623
freeman-lab <the.freeman.lab@gmail.com>
2021-09-14 17:22:14 -0700

    fix date formatting

--- components/utils/format-date.js
+++ components/utils/format-date.js
@@ -1,7 +1,7 @@
 export function formatDate(dateString, options) {
   const dateFormatter = new Intl.DateTimeFormat('en-US', options)
   const [month, day, year] = dateString.split('-')
-  const formattedDateString = `${year}-${month}-${day}`
+  const formattedDateString = `${year}-${month}-${parseInt(day) + 1}`
 
   return dateFormatter.format(new Date(formattedDateString)).replace(',', '')
 }
