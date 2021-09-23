614eb3c2503130b8abc82cec1f151888912da7a4
Kata Martin <katasm@gmail.com>
2021-09-23 16:00:20 -0700

    Add height/width on containers for maps

--- posts/maps-release/MapDemo2d.js
+++ posts/maps-release/MapDemo2d.js
@@ -5,7 +5,15 @@ import { useColormap } from '@carbonplan/colormaps'
 const MapDemo2d = () => {
   const colormap = useColormap('warm')
   return (
-    <Box as='figure' sx={{ mt: [6, 6, 6, 7], mb: [4, 4, 4, 5] }}>
+    <Box
+      as='figure'
+      sx={{
+        mt: [6, 6, 6, 7],
+        mb: [4, 4, 4, 5],
+        width: '100%',
+        height: '300px',
+      }}
+    >
       <Map>
         <Raster
           colormap={colormap}
