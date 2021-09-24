f97bf1f01698ee9b2861d3cba49f7173207dd85e
Kata Martin <katasm@gmail.com>
2021-09-24 10:24:43 -0700

    Add parameters to 4d example

--- posts/maps-release/MapDemo4d.js
+++ posts/maps-release/MapDemo4d.js
@@ -1,36 +1,63 @@
 import { Box } from 'theme-ui'
+import { useState } from 'react'
 import { Map, Raster } from '@carbonplan/maps'
 import { useColormap } from '@carbonplan/colormaps'
+import { Group } from '@carbonplan/components'
 import Coastlines from './Coastlines'
 import style from './style'
+import Parameters from './Parameters'
+
+const CLIMS = {
+  tavg: [-20, 30],
+  prec: [-100, 200],
+}
+
+const COLORMAPS = {
+  tavg: 'warm',
+  prec: 'cool',
+}
 
 const MapDemo4d = () => {
-  const colormap = useColormap('warm')
+  const [band, setBand] = useState('tavg')
+  const [month, setMonth] = useState(1)
+
+  const colormap = useColormap(COLORMAPS[band])
+
   return (
-    <Box
-      as='figure'
-      sx={{
-        my: [6, 6, 6, 7],
-        width: '100%',
-        height: '350px',
-        border: 'solid',
-        borderColor: 'muted',
-        borderWidth: '1px',
-      }}
-    >
-      <Map style={style}>
-        <Coastlines />
-        <Raster
-          colormap={colormap}
-          clim={[-20, 30]}
-          source={
-            'https://storage.googleapis.com/carbonplan-share/testing/maps/4d/tavg-prec-month'
-          }
-          variable={'climate'}
-          dimensions={['band', 'month', 'y', 'x']}
-          selector={{ band: 'tavg', month: 4 }}
+    <Box sx={{ my: [6, 6, 6, 7] }}>
+      <Group spacing='sm'>
+        <Box
+          as='figure'
+          sx={{
+            width: '100%',
+            height: '350px',
+            border: 'solid',
+            borderColor: 'muted',
+            borderWidth: '1px',
+          }}
+        >
+          <Map style={style}>
+            <Coastlines />
+            <Raster
+              colormap={colormap}
+              clim={CLIMS[band]}
+              source={
+                'https://storage.googleapis.com/carbonplan-share/testing/maps/4d/tavg-prec-month'
+              }
+              variable={'climate'}
+              dimensions={['band', 'month', 'y', 'x']}
+              selector={{ band, month }}
+            />
+          </Map>
+        </Box>
+
+        <Parameters
+          band={band}
+          setBand={setBand}
+          month={month}
+          setMonth={setMonth}
         />
-      </Map>
+      </Group>
     </Box>
   )
 }
---
+++ posts/maps-release/Parameters.js
@@ -0,0 +1,48 @@
+import { Box } from 'theme-ui'
+import { Badge, Filter, Group, Slider } from '@carbonplan/components'
+
+const sx = {
+  fontFamily: 'mono',
+  letterSpacing: 'mono',
+  textTransform: 'uppercase',
+}
+const Parameters = ({ band, setBand, month, setMonth }) => {
+  return (
+    <Group direction='horizontal'>
+      <Box sx={sx}>
+        Variable
+        <Filter
+          values={{ tavg: band === 'tavg', prec: band === 'prec' }}
+          setValues={(v) => setBand(v.tavg ? 'tavg' : 'prec')}
+        />
+      </Box>
+      <Box sx={sx}>
+        Month
+        <Box>
+          <Slider
+            min={1}
+            max={12}
+            step={1}
+            sx={{ width: '175px', display: 'inline-block' }}
+            value={month}
+            onChange={(e) => setMonth(parseFloat(e.target.value))}
+          />
+          <Badge
+            sx={{
+              bg: 'primary',
+              color: 'background',
+              display: 'inline-block',
+              position: 'relative',
+              ml: [3],
+              top: [1],
+            }}
+          >
+            {month.toFixed(0)}
+          </Badge>
+        </Box>
+      </Box>
+    </Group>
+  )
+}
+
+export default Parameters
