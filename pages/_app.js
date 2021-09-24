7135ee5b505714f6f445000783f3cfbc9bfe3cb2
Joseph Hamman <jhamman@ucar.edu>
2021-09-23 17:12:24 -0700

    add prism

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,17 @@ import { MDXProvider } from '@mdx-js/react'
 import '@carbonplan/components/fonts.css'
 import '@carbonplan/components/globals.css'
 import theme from '@carbonplan/theme'
+import { LiveCode, Pre } from '@carbonplan/prism'
+
+const components = {
+  code: LiveCode,
+  pre: Pre,
+}
 
 const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
-      <MDXProvider>
+      <MDXProvider components={components}>
         <Component {...pageProps} />
       </MDXProvider>
     </ThemeProvider>
