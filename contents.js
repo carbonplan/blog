9df351811334ab8d864cb95c4802e06d05c4ea89
freeman-lab <the.freeman.lab@gmail.com>
2021-09-27 12:56:59 -0700

    contents update

--- contents.js
+++ contents.js
@@ -4,7 +4,7 @@ const contents = [
     version: '1.0.0',
     title: 'A new toolkit for data-driven maps',
     authors: ['Jeremy Freeman', 'Kata Martin', 'Joe Hamman'],
-    date: '09-24-2021',
+    date: '09-27-2021',
     summary:
       'Why rendering scientific data in web maps is so tough, and how we’re trying to solve it',
     number: 2,
@@ -12,7 +12,7 @@ const contents = [
   {
     id: 'climate-trace-release',
     version: '1.0.0',
-    title: 'Climate TRACE released',
+    title: 'Climate TRACE release',
     authors: ['Oriana Chegwidden', 'Cindy Chiao', 'Joe Hamman'],
     date: '09-16-2021',
     summary:
