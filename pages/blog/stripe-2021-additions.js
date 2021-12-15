639f83600fdbaba1e6325246ee3e01a2a7fea0e3
freeman-lab <the.freeman.lab@gmail.com>
2021-12-15 10:27:16 -0800

    new post

---
+++ pages/blog/stripe-2021-additions.js
@@ -0,0 +1,10 @@
+import Index, { meta } from '../../posts/stripe-2021-additions.md'
+import Post from '../../components/post'
+
+const Content = () => (
+  <Post meta={meta} number={7}>
+    <Index />
+  </Post>
+)
+
+export default Content
