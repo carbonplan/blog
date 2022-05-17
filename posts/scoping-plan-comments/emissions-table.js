577169adeea8c6362082d37825485cb91a52aa05
freeman-lab <the.freeman.lab@gmail.com>
2022-05-17 14:10:19 -0700

    add new post

---
+++ posts/scoping-plan-comments/emissions-table.js
@@ -0,0 +1,34 @@
+import { Table } from '@carbonplan/components'
+
+const EmissionsTable = ({ columns, data }) => {
+  const titles = [
+    'Scenario',
+    'Net Zero date',
+    'CDR in 2045 (million tCO₂e)',
+    'CDR in 2045 (percent)',
+    'Emission Reductions in 2045 (percent)',
+  ]
+  return (
+    <Table
+      columns={[4, 5, 5, 5]}
+      index={false}
+      start={[
+        [1, 1, 1, 1],
+        [1, 2, 2, 2],
+        [2, 3, 3, 3],
+        [3, 4, 4, 4],
+        [4, 5, 5, 5],
+      ]}
+      width={[
+        [4, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+      ]}
+      data={[titles].concat(data)}
+    />
+  )
+}
+
+export default EmissionsTable
