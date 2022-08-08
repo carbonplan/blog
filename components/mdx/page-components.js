ada9f387a40b1b3d2b45cd83b1bf5409fbb1c6fd
katamartin <katasm@gmail.com>
2022-08-08 10:56:28 -0700

    Add SciPy 2022 post

--- components/mdx/page-components.js
+++ components/mdx/page-components.js
@@ -3,6 +3,7 @@ import dynamic from 'next/dynamic'
 // NOTE: This is a dynamically generated file based on the config specified under the
 //       `components` key in each post's frontmatter.
 const components = {
+  'scipy-conference-2022': {},
   'sec-offset-disclosure': {},
   'offsets-fires-update': {
     CreditingTable: dynamic(() =>
