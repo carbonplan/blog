6723c985322e71fbb1ef845994f29a426fb847ec
katamartin <katasm@gmail.com>
2023-12-12 15:16:49 -0500

    Add new post

--- components/mdx/page-components.js
+++ components/mdx/page-components.js
@@ -3,6 +3,7 @@ import dynamic from 'next/dynamic'
 // NOTE: This is a dynamically generated file based on the config specified under the
 //       `components` key in each post's frontmatter.
 const components = {
+  'zarr-visualization-update': {},
   'buffer-update-three': {},
   'vcl-update-v2023.10.0': {},
   'bigcoast-project-boundary': {
