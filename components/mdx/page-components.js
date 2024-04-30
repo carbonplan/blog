08ab9fa5e929188b8d33aed37ff026422cda7e89
katamartin <katasm@gmail.com>
2024-04-30 13:17:43 -0400

    Add new post

--- components/mdx/page-components.js
+++ components/mdx/page-components.js
@@ -3,6 +3,7 @@ import dynamic from 'next/dynamic'
 // NOTE: This is a dynamically generated file based on the config specified under the
 //       `components` key in each post's frontmatter.
 const components = {
+  'rimba-raya-license': {},
   'carb-buffer-decline': {
     BufferBalance: dynamic(() =>
       import('../../posts/carb-buffer-decline/buffer-balance.js').then(
