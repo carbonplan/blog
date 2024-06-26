d6f2f8096c37567076dc879f87f09809d8770261
Kata Martin <katasm@gmail.com>
2024-06-26 16:11:45 -0400

    Add new blog post (#165)

--- components/mdx/page-components.js
+++ components/mdx/page-components.js
@@ -3,6 +3,7 @@ import dynamic from 'next/dynamic'
 // NOTE: This is a dynamically generated file based on the config specified under the
 //       `components` key in each post's frontmatter.
 const components = {
+  'mescalero-wildfires-2024': {},
   'rimba-raya-license': {},
   'carb-buffer-decline': {
     BufferBalance: dynamic(() =>
