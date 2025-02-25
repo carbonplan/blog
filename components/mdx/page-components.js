cd226a100c888d01f83251445a59502bee607ac1
Kata Martin <katasm@gmail.com>
2025-02-25 09:52:21 -0500

    Add new post

--- components/mdx/page-components.js
+++ components/mdx/page-components.js
@@ -3,6 +3,7 @@ import dynamic from 'next/dynamic'
 // NOTE: This is a dynamically generated file based on the config specified under the
 //       `components` key in each post's frontmatter.
 const components = {
+  'ethanol-cdr-update': {},
   'compliance-users-v4': {},
   'klamath-east-relisting': {},
   'ipcc-cdr-methodologies': {},
