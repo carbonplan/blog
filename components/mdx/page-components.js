95c9c1f58ba9f71178e507794b716a34566e8063
Kata Martin <katasm@gmail.com>
2025-07-31 13:15:36 -0400

    Add autogen

--- components/mdx/page-components.js
+++ components/mdx/page-components.js
@@ -3,6 +3,7 @@ import dynamic from 'next/dynamic'
 // NOTE: This is a dynamically generated file based on the config specified under the
 //       `components` key in each post's frontmatter.
 const components = {
+  'offset-fires-2025': {},
   'aglime-accounting-workshop': {},
   'cdrxiv-launch-announcement': {},
   'offsets-db-additions': {
