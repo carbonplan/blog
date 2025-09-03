2c47b7476864b36cc0c317c3babe7227311d660d
badgley <graysonbadgley@gmail.com>
2025-09-03 09:57:31 -0700

    Add post about fifth anniversary of lionshead fire

--- components/mdx/page-components.js
+++ components/mdx/page-components.js
@@ -3,6 +3,7 @@ import dynamic from 'next/dynamic'
 // NOTE: This is a dynamically generated file based on the config specified under the
 //       `components` key in each post's frontmatter.
 const components = {
+  'lionshead-fifth-anniversary': {},
   'cdrxiv-open-software': {},
   'offset-fires-2025': {},
   'aglime-accounting-workshop': {},
